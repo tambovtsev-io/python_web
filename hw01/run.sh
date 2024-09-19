722613ba017fc66f7b2333f97614b8c9e520d438
ilia <tambovtsev.io@phystech.edu>
2024-09-19 20:38:01 +0300

    fix killing server after execution

--- hw01/run.sh
+++ hw01/run.sh
@@ -2,7 +2,7 @@
 
 # Start the ASGI server in the background
 echo "Starting the ASGI server..."
-poetry run uvicorn simple_math_asgi:SimpleMathASGIServer --host 127.0.0.1 --port 8000 & server_pid=$!
+poetry run uvicorn simple_math_asgi:SimpleMathASGIServer --host 127.0.0.1 --port 8000 &
 
 # Wait a bit to ensure the server is running
 sleep 4
@@ -13,7 +13,7 @@ poetry run pytest test_hw01.py
 
 # After the tests finish, kill the server
 echo "Stopping the ASGI server..."
-kill $server_pid
+pkill -9 uvicorn
 
 # Exit the script
 exit 0
