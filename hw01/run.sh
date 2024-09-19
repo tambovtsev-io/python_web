c38c8d3d7c8cac90f66cb3289550cb638329ce9f
ilia <why6mail@gmail.com>
2024-09-19 17:30:30 +0300

    add run.sh

---
+++ hw01/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Start the ASGI server in the background
+echo "Starting the ASGI server..."
+poetry run uvicorn simple_math_asgi:SimpleMathASGIServer --host 127.0.0.1 --port 8000 & server_pid=$!
+
+# Wait a bit to ensure the server is running
+sleep 4
+
+# Run pytest for the test.py file
+echo "Running tests..."
+poetry run pytest test_hw01.py
+
+# After the tests finish, kill the server
+echo "Stopping the ASGI server..."
+kill $server_pid
+
+# Exit the script
+exit 0
